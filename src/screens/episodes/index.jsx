52557440de4643a6baf5366ae2223878848961d2
erenbedir6161-web <erenbedir6161@gmail.com>
2026-09-03 20:19:00 +0300

    Güncellemeler yapıldı

---
+++ src/screens/episodes/index.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const episodes = () => {
+  return (
+    <View>
+      <Text>Episodes</Text>
+    </View>
+  )
+}
+
+export default Characters
+
+const styles = StyleSheet.create({})
---
+++ src/styles/tabBarStyle.js
@@ -0,0 +1,13 @@
+import { StyleSheet } from 'react-native'
+import Colors from '../theme/colors'
+
+const tabBarStyle = StyleSheet.create({
+  headerStyle: {
+    backgrounColor: Colors.BACKGROUNDCOLOR
+  },
+  tabBarStyle:{
+    backgroundColor: Colors.BACKGROUNDCOLOR,
+  }
+})
+
+export default tabBarStyle
